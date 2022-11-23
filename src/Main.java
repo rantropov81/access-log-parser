88e01f5e5dde32b0cd2dc8bc561d9d1836372363
Roman Antropov <rantropov@gmail.com>
2022-11-23 23:52:14 +0300

    Text input + text length

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println("Введите число:");
+        int number = new Scanner(System.in).nextInt();
+        int number2 = new Scanner(System.in).nextInt();
+        int mult = number * number2;
+        int sum = number + number2;
+        int dif = number - number2;
+        double div = (double) number / number2;
+
     }
 }
