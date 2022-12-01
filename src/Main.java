78d5d653269c0341b27c40058615dbd9c3532651
Roman Antropov <rantropov@gmail.com>
2022-12-01 13:14:19 +0300

    Запрос пути а файлу и проверка на существование такого файла, диретории. Подсчет правильно указанных файлов.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int num1 = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int num2 = new Scanner(System.in).nextInt();
-        System.out.println("Сложение: " + (num1+num2));
-        System.out.println("Вычитание: " + (num1-num2));
-        System.out.println("Произведение: " + num1*num2);
-        System.out.println("Частное: " + (double)num1/num2);
+
+            int x=1;
+            int n=0;
+            while (x==1) {
+                System.out.println("ведите путь к файлу: ");
+                String path = new Scanner(System.in).nextLine();
+                File file = new File(path);
+                boolean fileExists = file.exists();
+                boolean isDirectory = file.isDirectory();
+                if(!fileExists || isDirectory) {
+                    System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
+                    continue;
+                }
+                if(fileExists) {
+                    n++;
+                    System.out.println("Путь указан верно");
+                    System.out.println("Это файл номер "+n);
+            }
+        }
     }
     }
 
