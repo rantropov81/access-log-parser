a4c999c187c5c4f37326885bb731349e04f267f3
Roman Antropov <rantropov@gmail.com>
2023-02-26 02:45:39 +0300

    Добавлены Statistics, LogEntry, UserAgent.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,38 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String path;
+        Statistics statistics = new Statistics();
+        int count = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            path = new Scanner(System.in).nextLine();
+            File file = new File(path);
 
-            int x=1;
-            int n=0;
-            while (x==1) {
-                System.out.println("ведите путь к файлу: ");
-                String path = new Scanner(System.in).nextLine();
-                File file = new File(path);
-                boolean fileExists = file.exists();
-                boolean isDirectory = file.isDirectory();
-                if(!fileExists || isDirectory) {
-                    System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
-                    continue;
+            boolean pathOfFile = file.exists();
+            boolean isNotDirectory = !file.isDirectory();
+            if (pathOfFile && isNotDirectory) {
+                System.out.println("Путь к файлу правильный");
+                System.out.println("Это файл №" + ++count);
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader =
+                            new BufferedReader(fileReader);
+
+                    System.out.println("Total traffic: " + statistics.totalTraffic + " bytes");
+                    System.out.println("Min time: " + statistics.minTime);
+                    System.out.println("Max time: " + statistics.maxTime);
+                    System.out.println("Traffic rate: " + statistics.getTrafficRate() + " bytes per hour");
+                } catch (IOException e) {
+                    System.out.println(e.fillInStackTrace());
                 }
-                if(fileExists) {
-                    n++;
-                    System.out.println("Путь указан верно");
-                    System.out.println("Это файл номер "+n);
+
+
+            } else {
+                System.out.println("Файл отсутствует или путь ведет к директории файла");
             }
         }
     }
-    }
-
+}
