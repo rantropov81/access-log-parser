8a0661b5ff8b054156b91729b1f2d856c82ca341
Roman Antropov <rantropov@gmail.com>
2023-03-11 14:01:01 +0300

    Добавлены Statistics, LogEntry, UserAgent.

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,31 @@ public class Main {
         Statistics statistics = new Statistics();
         int count = 0;
         while (true) {
-            System.out.println("Введите путь к файлу:");
+            System.out.println("Введите путь к файлу и нажмите <Enter>:");
             path = new Scanner(System.in).nextLine();
             File file = new File(path);
 
             boolean pathOfFile = file.exists();
             boolean isNotDirectory = !file.isDirectory();
             if (pathOfFile && isNotDirectory) {
-                System.out.println("Путь к файлу правильный");
+                System.out.println("Путь к файлу является правильным");
                 System.out.println("Это файл №" + ++count);
                 try {
                     FileReader fileReader = new FileReader(path);
                     BufferedReader reader =
                             new BufferedReader(fileReader);
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+
+                        if (length > 1024) {
+                            throw new RuntimeException("Length of line > 1024");
+                        }
+
+                        LogEntry e = new LogEntry(line);
+                        statistics.addEntry(e);
+                    }
 
                     System.out.println("Total traffic: " + statistics.totalTraffic + " bytes");
                     System.out.println("Min time: " + statistics.minTime);
